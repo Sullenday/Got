2a119ce7ba6fb0178867dd3acd9882b014ad4a94
denis <denis@DESKTOP-3NJGCDV>
2023-07-26 20:46:43 +1000

    Добавьте файлы проекта.

---
+++ Got.cpp
@@ -0,0 +1,432 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
+#include <vector>
+using namespace std;
+
+class Logic {
+public:
+    int life[81][2];
+    int dots[81][2];
+    int board[9][9];
+    int coun = 0;
+
+    void init() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    void isEnd() {
+        system("cls");
+        for (int i1 = 0; i1 < 9; i1++) {
+            for (int j1 = 0; j1 < 9; j1++) {
+                if (board[i1][j1] == 1 || board[i1][j1] == 2) {
+                    clean();
+                    breath(i1, j1);
+                    capture();
+                    clean();
+                }
+                if (board[i1][j1] == 0) {
+                    clean();
+                    freeland(i1, j1, 1, 0);
+                    freelandconquer();
+                    clean();
+                    freeland(i1, j1, 2, 0);
+                    freelandconquer();
+                    clean();
+                }
+            }
+        }
+        for (int i1 = 0; i1 < 9; i1++) {
+            for (int j1 = 0; j1 < 9; j1++) {
+                cout << board[i1][j1] << " ";
+            }
+            cout << endl;
+        }
+        cout << "Игра окончена\n";
+        cout << "Счет первого игрока" << score(1) << endl << "Счет второго игрока " << score(2) << endl;
+        if (score(1) > score(2)) {
+            cout << "Побеждает первый игрок\n";
+        }
+        else if (score(2) > score(1)) {
+            cout << "Побеждает второй игрок\n";
+        }
+        else {
+            cout << "Ничья!!!\n";
+        }
+        cout << "Для рестарта нажмите R";
+        int key = _getch();
+        if (key == 114) {
+            reset();
+        }
+    }
+
+    void reset() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    void clean() {
+        for (int i = 0; i < 81; i++) {
+            for (int j = 0; j < 2; j++) {
+                life[i][j] = -1;
+                dots[i][j] = -1;
+            }
+        }
+        coun = 0;
+    }
+
+    void breath(int i, int j) {
+        bool flag = true;
+        for (int i1 = 0; i1 < 81; i1++) {
+            if ((i == dots[i1][0]) && (j == dots[i1][1])) {
+                flag = false;
+            }
+        }
+        if (flag == true) {
+            dots[coun][0] = i;
+            dots[coun][1] = j;
+            coun++;
+            if (i != 0) {
+                if ((board[i - 1][j] == 0) || (board[i - 1][j] == -board[i][j])) {
+                    life[coun][0] = i - 1;
+                    life[coun][1] = j;
+                }
+                else if (board[i - 1][j] == board[i][j]) {
+                    breath(i - 1, j);
+                }
+            }
+            if (i != 8) {
+                if ((board[i + 1][j] == 0) || (board[i + 1][j] == -board[i][j])) {
+                    life[coun][0] = i + 1;
+                    life[coun][1] = j;
+                }
+                else if (board[i + 1][j] == board[i][j]) {
+                    breath(i + 1, j);
+                }
+            }
+            if (j != 0) {
+                if ((board[i][j - 1] == 0) || (board[i][j - 1] == -board[i][j])) {
+                    life[coun][0] = i;
+                    life[coun][1] = j - 1;
+                }
+                else if (board[i][j - 1] == board[i][j]) {
+                    breath(i, j - 1);
+                }
+            }
+            if (j != 8) {
+                if ((board[i][j + 1] == 0) || (board[i][j + 1] == -board[i][j])) {
+                    life[coun][0] = i;
+                    life[coun][1] = j + 1;
+                }
+                else if (board[i][j + 1] == board[i][j]) {
+                    breath(i, j + 1);
+                }
+            }
+        }
+    }
+
+    void capture() {
+        bool flag = true;
+        for (int i = 0; i < 81; i++) {
+            if ((life[i][0] != -1) && (life[i][1] != -1)) {
+                flag = false;
+            }
+        }
+        if (flag == true) {
+            int oppos;
+            if (board[dots[0][0]][dots[0][1]] == 1) {
+                oppos = 2;
+            }
+            else {
+                oppos = 1;
+            }
+            for (int i = 0; i < coun; i++) {
+                board[dots[i][0]][dots[i][1]] = -oppos;
+            }
+            clean();
+        }
+    }
+
+
+    void freeland(int i, int j, int whoturn, int coun2) {
+        bool flag = true;
+        for (int i1 = 0; i1 < 81; i1++) {
+            if ((i == dots[i1][0]) && (j == dots[i1][1])) {
+                flag = false;
+            }
+        }
+        if (flag == true) {
+            int oppos;
+            if (whoturn == 1) {
+                oppos = 2;
+            }
+            else {
+                oppos = 1;
+            }
+            dots[coun][0] = i;
+            dots[coun][1] = j;
+            coun++;
+            if (i != 0) {
+                if ((board[i - 1][j] == 0) || (board[i - 1][j] == -board[i][j])) {
+                    freeland(i - 1, j, whoturn, coun2);
+                }
+                else if (board[i - 1][j] == whoturn) {
+                    life[coun2][0] = i - 1;
+                    life[coun2][1] = j;
+                    coun2++;
+                }
+            }
+            if (i != 8) {
+                if ((board[i + 1][j] == 0) || (board[i + 1][j] == -board[i][j])) {
+                    freeland(i + 1, j, whoturn, coun2);
+                }
+                else if (board[i + 1][j] == whoturn) {
+                    life[coun2][0] = i + 1;
+                    life[coun2][1] = j;
+                    coun2++;
+                }
+            }
+            if (j != 0) {
+                if ((board[i][j - 1] == 0) || (board[i][j - 1] == -board[i][j])) {
+                    freeland(i, j - 1, whoturn, coun2);
+                }
+                else if (board[i][j - 1] == whoturn) {
+                    life[coun2][0] = i;
+                    life[coun2][1] = j - 1;
+                    coun2++;
+                }
+            }
+            if (j != 8) {
+                if ((board[i][j + 1] == 0) || (board[i][j + 1] == -board[i][j])) {
+                    freeland(i, j + 1, whoturn, coun2);
+                }
+                else if (board[i][j + 1] == whoturn) {
+                    life[coun2][0] = i;
+                    life[coun2][1] = j + 1;
+                    coun2++;
+                }
+            }
+        }
+    }
+
+    void freelandconquer() {
+        int counter = 0;
+        for (int i = 0; i < 81; i++) {
+            if (life[i][0] != -1) {
+                counter++;
+            }
+        }
+        if ((counter >= coun / 3) && (coun < 35)) {
+            bool flag = false;
+            for (int i = 0; i < 81; i++) {
+                if (dots[i][0] == 0) {
+                    flag = true;
+                }
+            }
+            for (int i = 0; i < 81; i++) {
+                if ((life[i][0] == 0) || (life[i][1] == 0) || (life[i][1] == 8)) {
+                    flag = false;
+                }
+            }
+            if (!flag) {
+                for (int i = 0; i < 81; i++) {
+                    if (dots[i][0] == 8) {
+                        flag = true;
+                    }
+                }
+                for (int i = 0; i < 81; i++) {
+                    if ((life[i][0] == 8) || (life[i][1] == 0) || (life[i][1] == 8)) {
+                        flag = false;
+                    }
+                }
+                if (!flag) {
+                    for (int i = 0; i < 81; i++) {
+                        if (dots[i][1] == 0) {
+                            flag = true;
+                        }
+                    }
+                    for (int i = 0; i < 81; i++) {
+                        if ((life[i][0] == 8) || (life[i][1] == 0) || (life[i][0] == 0)) {
+                            flag = false;
+                        }
+                    }
+                    if (!flag) {
+                        for (int i = 0; i < 81; i++) {
+                            if (dots[i][1] == 8) {
+                                flag = true;
+                            }
+                        }
+                        for (int i = 0; i < 81; i++) {
+                            if ((life[i][0] == 8) || (life[i][1] == 8) || (life[i][0] == 0)) {
+                                flag = false;
+                            }
+                        }
+                        if (!flag) {
+                            for (int i = 0; i < counter - 1; i++) {
+                                int schet = 1;
+                                for (int j = i + 1; j < counter; j++) {
+                                    if ((life[i][0] == life[j][0]) && (life[i][1] == life[j][1])) {
+                                        schet += 1;
+                                    }
+                                }
+                                if (schet > 2) {
+                                    flag = true;
+                                }
+                                schet = 1;
+                            }
+                            if (!flag) {
+                                for (int i = 0; i < coun; i++) {
+                                    board[dots[i][0]][dots[i][1]] = -board[life[0][0]][life[0][1]];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        clean();
+    }
+
+    int score(int player) {
+        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == -player) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    void game() {
+        setlocale(LC_ALL, "Russian");
+        cout << "Управление стрелками, чтобы сбросить поле нажмите R \n z - спасовать ход (2 паса подряд - ничья) \n У каждого игрока по 20 комней, но использовать их все не обязательно \n Счет очков идет по захваченным территория, отмеченым в конце номером игрока умноженым на -1";
+        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(handle, FOREGROUND_RED);
+        int key, stone1 = 20, stone2 = 20, pas = 0, oppos = 1, whoturn = 2, i = 4, j = 4;
+        while (true) {
+            key = _getch();
+            cout << "Нажимайте кнопки, чтобы выбрать ячейки";
+            while ((key != 13) && (key != 122) && (key != 239)) {
+                system("cls");
+                switch (key)
+                {
+                case 75: if (j != 0) {
+                    j--;
+                }
+                       else {
+                    cout << "Перемещение влево невозможно\n";
+                }
+                       break;
+                case 72: if (i != 0) { i--; }
+                       else {
+                    cout << "Перемещение вверх невозможно\n";
+                }
+                       break;
+                case 77: if (j != 8) { j++; }
+                       else {
+                    cout << "Перемещение вправо невозможно\n";
+                }
+                       break;
+                case 80: if (i != 8) { i++; }
+                       else {
+                    cout << "Перемещение вниз невозможно\n";
+                }
+                       break;
+                case 114: reset();
+                    break;
+                case 170: reset();
+                    break;
+                }
+
+                for (int i1 = 0; i1 < 9; i1++) {
+                    for (int j1 = 0; j1 < 9; j1++) {
+                        if ((i1 == i) && (j1 == j)) {
+                            SetConsoleTextAttribute(handle, FOREGROUND_GREEN);
+                            cout << board[i1][j1] << " ";
+                            SetConsoleTextAttribute(handle, FOREGROUND_RED);
+                        }
+                        else { cout << board[i1][j1] << " "; }
+                    }
+                    cout << endl;
+                }
+                cout << "-------------------------------------------------------\n Ходит игрок номер " << whoturn;
+                key = _getch();
+            }
+            if ((key == 239) || (key == 122)) {
+                pas++;
+                switch (whoturn) {
+                case 1: whoturn++;
+                    oppos--;
+                    break;
+                case 2: whoturn--;
+                    oppos++;
+                    break;
+                }
+                if (pas == 2) {
+                    isEnd();
+                }
+            }
+            else {
+                pas = 0;
+                if ((board[i][j] == 0) || (board[i][j] == -whoturn)) {
+                    board[i][j] = whoturn;
+                    for (int i1 = 0; i1 < 9; i1++) {
+                        for (int j1 = 0; j1 < 9; j1++) {
+                            if (board[i1][j1] == 1 || board[i1][j1] == 2) {
+                                clean();
+                                breath(i1, j1);
+                                capture();
+                                clean();
+                            }
+                        }
+                    }
+                    system("cls");
+                    for (int i1 = 0; i1 < 9; i1++) {
+                        for (int j1 = 0; j1 < 9; j1++) {
+                            cout << board[i1][j1] << " ";
+                        }
+                        cout << endl;
+                    }
+                    key = _getch();
+                    switch (whoturn) {
+                    case 1: stone1--;
+                        if (stone2 != 0) {
+                            whoturn++;
+                            oppos--;
+                        }
+                        break;
+                    case 2: stone2--;
+                        if (stone1 != 0) {
+                            whoturn--;
+                            oppos++;
+                        }
+                        break;
+                    }
+                    if ((stone1 == 0) && (stone2 == 0)) {
+                        isEnd();
+                    }
+                }
+                else {
+                    cout << "Нельзя ставить на занятую уже занятую ячейчку, поставьте в не занятую" << endl;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Logic Go;
+    Go.init();
+    Go.game();
+}
